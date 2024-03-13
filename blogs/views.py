3ed1469a15acc2b33d298d5a2f1af63ab285c617
Ken.Mulla <kenm2000@gmail.com>
2024-03-13 15:32:04 -0400

    blogs and search features

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from . models import Blog, Category
+from django.db.models import Q
 
 
 
@@ -17,3 +18,21 @@ def posts_by_category(request, category_id):
         'category_id': category,
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {
+        'single_blog': single_blog,
+    }
+    return render(request, 'blogs.html', context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_descripiton__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
